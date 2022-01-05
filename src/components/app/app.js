0709189d2925d8bc536d3df3740cee9ba3bc8df3
Andrew <andreialekseevich51@gmail.com>
2022-01-05 15:16:10 +0300

    lifeCircle

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,14 +1,91 @@
-import React from "react";
-import SwapiService from "../../services/swapi-service";
+// import React from 'react';
 
-export const App = () => {
-  const swapi = new SwapiService();
+// import Header from '../header';
+// import RandomPlanet from '../random-planet';
+// import ItemList from '../item-list';
+// import PersonDetails from '../person-details';
 
-  swapi.getAllPeople().then((people) => {
-    people.forEach((p) => {
-      console.log(p.name);
+// import './app.css';
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Header />
+//       <RandomPlanet />
+
+//       <div className="row mb2">
+//         <div className="col-md-6">
+//           <ItemList />
+//         </div>
+//         <div className="col-md-6">
+//           <PersonDetails />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+
+import React, { Component } from 'react';
+
+import Header from '../header';
+import RandomPlanet from '../random-planet';
+import ItemList from '../item-list';
+import PersonDetails from '../person-details';
+
+import './app.css';
+
+export default class App extends Component {
+
+  state = {
+    showRandomPlanet: true,
+    selectedPerson: 5
+  };
+
+  toggleRandomPlanet = () => {
+    this.setState((state) => {
+      return {
+        showRandomPlanet: !state.showRandomPlanet
+      }
     });
-  });
-  
-  return <div></div>;
-};
+  };
+
+  onPersonSelected = (id) => {
+    this.setState({
+      selectedPerson: id
+    });
+  };
+
+  render() {
+
+    const planet = this.state.showRandomPlanet ?
+      <RandomPlanet/> :
+      null;
+
+    return (
+      <div className="stardb-app">
+        <Header />
+        { planet }
+
+        <button
+          className="toggle-planet btn btn-warning btn-lg"
+          onClick={this.toggleRandomPlanet}>
+          Toggle Random Planet
+        </button>
+
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList onItemSelected={this.onPersonSelected}/>
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+// export default App;
+
+
